2a851eef424a4b662fd7073b292a7fa2db209aaa
Jain-Sanchit <Jainsanchit157@gmail.com>
2020-06-21 00:07:24 +0530

    changes

---
+++ client/build/precache-manifest.9982730f1303529ce2c9c0754781883f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ef30cff5f4f93ba319ce64bdcfe0bed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d5585735f7076575271",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "7c3d964c2f234e6074d9",
+    "url": "/static/js/2.78f9918b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.78f9918b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d5585735f7076575271",
+    "url": "/static/js/main.bdbd0f9e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
